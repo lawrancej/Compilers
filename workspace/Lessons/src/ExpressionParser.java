8ee384de9ada7b71c655bd6c5f1db37c01f521bf
Joey Lawrance <lawrancej@wit.edu>
2013-07-01 08:16:16 -0400

    Added Expression parser primitives to Lessons.

---
+++ workspace/Lessons/src/ExpressionParser.java
@@ -0,0 +1,36 @@
+
+public class ExpressionParser {
+	String input;
+	int i = 0;
+	public ExpressionParser(String input) {
+		this.input = input;
+	}
+	// Consume a character (this assumes we don't have a scanner/lexer)
+	private void eat(char c) throws Exception 
+	{
+		// If the characters match, advance
+		if (input.charAt(0) == c) {
+			input = input.substring(1);
+			i++;
+		}
+		// If not, then we just throw up
+		else {
+			throw new Exception (String.format("Expected %c but got %c at position %d", c, input.charAt(0), i));
+		}
+	}
+	// Is there anything left?
+	private boolean more () {
+		return input.length() > 0;
+	}
+	// Look ahead
+	private char peek() {
+		return input.charAt(0);
+	}
+	// Advance to the next character
+	private char next() throws Exception {
+		char c = peek() ;
+		eat(c) ;
+		return c ;
+	}
+	
+}
