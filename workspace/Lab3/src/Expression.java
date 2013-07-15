6722985136f74ba89766f9cf81edeb863714c48f
Joey Lawrance <lawrancej@wit.edu>
2013-07-15 13:25:02 -0400

    Added starter code for choice two of Lab 3.

--- workspace/Lab3/src/Expression.java
+++ workspace/Lab3/src/Expression.java
@@ -3,10 +3,10 @@ import static library.Combinators.*;
 
 public class Expression {
 	public static Parser grammar() {
-		Nonterminal group = new Nonterminal();
-		Nonterminal factor = new Nonterminal();
-		Nonterminal term = new Nonterminal();
-		Nonterminal expression = new Nonterminal();
+		Nonterminal group = new Nonterminal("group");
+		Nonterminal factor = new Nonterminal("factor");
+		Nonterminal term = new Nonterminal("term");
+		Nonterminal expression = new Nonterminal("expression");
 		
 		// group       ::= '(' expression ')'
 		group.derives(symbol('('), expression, symbol(')'));
@@ -16,6 +16,10 @@ public class Expression {
 		term.derives(factor, star(or(seq(symbol('*'), factor), seq(symbol('/'), factor))));
 		// expression  ::= term (('+' term) | ('-' term))*
 		expression.derives(term, star(or(seq(symbol('+'), term), seq(symbol('-'), term))));
-		return expression;
+		return new Grammar(expression);
+	}
+	public static void main(String[] args) {
+		Printer printer = new Printer();
+		System.out.println(grammar().accept(printer));
 	}
 }
--- workspace/Lab3/src/library/Combinators.java
+++ workspace/Lab3/src/library/Combinators.java
@@ -1,5 +1,8 @@
 package library;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Combinators {
 	String input;
 	int i = 0;
@@ -33,24 +36,50 @@ public class Combinators {
 	private char next() throws Exception {
 		char c = peek() ;
 		eat(c) ;
-		return c ;
+		return c;
 	}
 	// Parser combinators
 	public static interface Visitor<T> {
-		public T visit(Nonterminal p);
-		public T visit(Terminal p);
+		public T visit(Grammar g);
+		public T visit(Nonterminal n);
+		public T visit(Terminal t);
+		public T visit(Sequence s);
+		public T visit(Or o);
+		public T visit(Star s);
 	}
+	// This is like the Node class in the other labs
 	public static interface Parser {
-		
+		public <T> T accept(Visitor<T> visitor);
 	}
+	/*
+	 * Grammar construction classes
+	 */
+	// Nonterminal represents the productions of a nonterminal.
 	public static class Nonterminal implements Parser {
-
-		public void derives(Parser...parsers) {
+		String name;
+		Parser rhs;
+		public Nonterminal() {
 			
+		}
+		public Nonterminal(String name) {
+			this.name = name;
+		}
+		public void derives(Parser...parsers) {
+			rhs = new Sequence(parsers);
+		}
+		@Override
+		public <T> T accept(Visitor<T> visitor) {
+			return visitor.visit(this);
 		}
 
 	}
+	// Represe
 	public static class Terminal implements Parser {
+
+		@Override
+		public <T> T accept(Visitor<T> visitor) {
+			return visitor.visit(this);
+		}
 	}
 	private static class Symbol extends Terminal {
 		char c;
@@ -58,20 +87,165 @@ public class Combinators {
 			this.c = c;
 		}
 	}
+	private static class Sequence implements Parser {
+		Parser[] parsers;
+		public Sequence(Parser...parsers) {
+			this.parsers = parsers;
+		}
+		@Override
+		public <T> T accept(Visitor<T> visitor) {
+			return visitor.visit(this);
+		}
+	}
+	private static class Or implements Parser {
+		Parser[] parsers;
+		public Or(Parser...parsers) {
+			this.parsers = parsers;
+		}
+		@Override
+		public <T> T accept(Visitor<T> visitor) {
+			return visitor.visit(this);
+		}
+	}
+	private static class Star implements Parser {
+		Parser child;
+		public Star(Parser child) {
+			this.child = child;
+		}
+		@Override
+		public <T> T accept(Visitor<T> visitor) {
+			return visitor.visit(this);
+		}
+	}
+	/*
+	 * Grammar construction convenience methods
+	 */
 	public static Parser symbol(char c) {
 		return new Symbol(c);
 	}
-	public static Parser star(Parser p) {
-		return p;
+	public static Parser star(Parser t) {
+		return new Star(t);
 	}
 	public static Parser seq(Parser...parsers) {
-		return null;
+		return new Sequence(parsers);
 	}
-	public static Parser or(Parser a, Parser b) {
-		return b;
+	public static Parser or(Parser...parsers) {
+		return new Or(parsers);
 		
 	}
 	public static Parser integer() {
-		return null;
+		return new Terminal();
+	}
+	public static class Grammar implements Parser {
+		Set<Nonterminal> nonterminals;
+		public Grammar(Nonterminal start) {
+			Builder builder = new Builder();
+			start.accept(builder);
+			nonterminals = builder.set;
+		}
+		private static class Builder implements Visitor<Void> {
+			public Set<Nonterminal> set;
+			public Builder() {
+				set = new HashSet<Nonterminal>();
+			}
+			@Override
+			public Void visit(Nonterminal n) {
+				if (!set.contains(n)) {
+					set.add(n);
+					n.rhs.accept(this);
+				}
+				return null;
+			}
+			@Override
+			public Void visit(Terminal t) {
+				return null;
+			}
+			@Override
+			public Void visit(Sequence s) {
+				for (Parser p : s.parsers) {
+					p.accept(this);
+				}
+				return null;
+			}
+			@Override
+			public Void visit(Or o) {
+				for (Parser p : o.parsers) {
+					p.accept(this);
+				}
+				return null;
+			}
+			@Override
+			public Void visit(Star s) {
+				// TODO Auto-generated method stub
+				s.child.accept(this);
+				return null;
+			}
+			@Override
+			public Void visit(Grammar g) {
+				return null;
+			}
+		}
+
+		@Override
+		public <T> T accept(Visitor<T> visitor) {
+			return visitor.visit(this);
+		}
+	}
+	public static class Printer implements Visitor<String> {
+
+		@Override
+		public String visit(Nonterminal n) {
+			return n.name;
+		}
+
+		@Override
+		public String visit(Terminal t) {
+			if (t instanceof Symbol) {
+				Symbol s = (Symbol) t;
+				return "'" + s.c + "'";
+			}
+			return "";
+		}
+
+		@Override
+		public String visit(Sequence s) {
+			StringBuilder builder = new StringBuilder();
+			for (Parser p : s.parsers) {
+				builder.append(" ");
+				builder.append(p.accept(this));
+			}
+			return builder.toString();
+		}
+
+		@Override
+		public String visit(Or o) {
+			StringBuilder builder = new StringBuilder();
+			for (int i = 0; i < o.parsers.length; i++) {
+				builder.append("(");
+				builder.append(o.parsers[i].accept(this));
+				builder.append(")");
+				if (i + 1 != o.parsers.length)
+					builder.append(" | ");
+			}
+			return builder.toString();
+		}
+
+		@Override
+		public String visit(Star s) {
+			return "(" + s.child.accept(this) + ")*";
+		}
+
+		@Override
+		public String visit(Grammar g) {
+			StringBuilder builder = new StringBuilder();
+			for (Nonterminal n : g.nonterminals) {
+				builder.append(n.name);
+				builder.append(" ::= ");
+				builder.append(n.rhs.accept(this));
+				builder.append("\n");
+			}
+			return builder.toString();
+		}
+		
 	}
 }
